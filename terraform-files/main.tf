eb0dc28a2b61938dec0a501c52beec208e1d0b5f
challadevops1 <124413798+challadevops1@users.noreply.github.com>
2024-06-07 08:18:57 +0530

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "Test-Servers" {
     Name = "Test-Servers"
   }
   provisioner "local-exec" {
-    command = "echo ${aws_instance.New-test-server.public_ip} > inventory"
+    command = "echo ${aws_instance.Test-Servers.public_ip} > inventory"
   }
   provisioner "local-exec" {
     command = "ansible-playbook /var/lib/jenkins/workspace/Banking-Project/terraform-files/ansibleplaybook.yml"
